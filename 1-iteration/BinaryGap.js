678ba46306e95a9a2e60a5a1e7afde26cd959cfb
ruperthnyagesoa <ruperthnyagesoa@gmail.com>
2023-07-14 04:18:23 +0300

    feat:BinaryGap

--- 1-iteration/BinaryGap.js
+++ 1-iteration/BinaryGap.js
@@ -5,20 +5,15 @@ function solution(N) {
     let binary = N.toString(2);
     let maxGap = 0;
     let currentGap = 0;
-  
-    for (let i = 1; i < binary.length - 1; i++) {
-        if (binary[i] === '0') {
-            currentGap++;
-        }else{
+    for (let i = 0; i < binary.length; i++) {
+        if (binary[i] === '1') {
             if (currentGap > maxGap) {
                 maxGap = currentGap;
             }
             currentGap = 0;
+        } else {
+            currentGap++;
         }
     }
-    if (maxGap > 0) {
-        return maxGap;
-    }else {
-        return 0;
-    }
-  }
+    return maxGap;
+}
